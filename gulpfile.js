95a0004207a0de62bd6d2ee1a6b33c447f54a7c6
Leo Feyer <github@contao.org>
2017-09-26 09:31:39 +0200

    Add a gulp taks to minify the assets.

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp'),
+    csso = require('gulp-csso'),
+    ignore = require('gulp-ignore'),
+    rename = require('gulp-rename'),
+    svgo = require('gulp-svgo'),
+    pump = require('pump');
+
+gulp.task('minify-css', function (cb) {
+    pump([
+            gulp.src('css/*.css'),
+            ignore.exclude('*.min.css'),
+            csso({
+                comments: false,
+                restructure: false
+            }),
+            rename({
+                suffix: '.min'
+            }),
+            gulp.dest('css')
+        ],
+        cb
+    );
+});
+
+gulp.task('minify-images', function (cb) {
+    pump([
+            gulp.src('images/*.svg'),
+            svgo(),
+            gulp.dest('images')
+        ],
+        cb
+    );
+});
+
+gulp.task('default', ['minify-css', 'minify-images']);
